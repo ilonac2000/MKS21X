2dff6f7824b1aa95de70716e55c74d44864e0e0c
Ilona <icherepakhina@stuy.edu>
2016-12-13 05:53:27 -0500

    A

---
+++ 04Sorts/.#Sorts.java
@@ -0,0 +1,1 @@
+ilona.cherepakhina@cslab4-28.3357:1480915630
--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -1,1 +1,19 @@
+public class Sorts{
+  public static String name(){
+    return "09.Cherepakhina.Ilona"; 
+  }
 
+  /**Selection sort of an int array. 
+  *Upon completion, the elements of the array will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
+  public static void selectionSort(int[] data){ 
+      for (int i = 0; i < data.length - 1 ; i++){
+	  min = data[i];
+	  for (int j = 0; j < data.length - 1; j++){
+	      if (min > data[j]);
+		  min = data[j];
+	  }
+      }
+
+}
