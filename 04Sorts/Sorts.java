e39fec8938da3cbe1f2c91dd5b9f408bd0adf87e
ilonac2000 <icherepakhina@stuy.edu>
2016-12-13 20:40:02 -0500

    Update Sorts.java

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -8,12 +8,25 @@ public class Sorts{
   *@param data  the elements to be sorted.
   */
   public static void selectionSort(int[] data){ 
-      for (int i = 0; i < data.length - 1 ; i++){
-	  min = data[i];
-	  for (int j = 0; j < data.length - 1; j++){
-	      if (min > data[j]);
-		  min = data[j];
-	  }
-      }
-
+    for (int i = 0; i < data.length; i++){
+	  int index = i;
+	  for (int j = 0; j < data.length; j++){
+	      if (data[index] < data[j]){
+		        index = j;
+        int min = data[index];
+        data[index] = data[i];
+        data[i] = min;
+          }
+	  }  
+  }
+}
+     
+/*public static void main(String []args){   
+        int[] a = {64,25,12,22,11};
+        selectionSort(a);
+        for(int i:a){
+            System.out.print(i);
+            System.out.print(", ");
+    }
+  }*/
 }
