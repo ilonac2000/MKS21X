b76f5f0bf32f9e8f4c3ad728eec5923a39bc13ef
ilonac2000 <icherepakhina@stuy.edu>
2016-12-22 19:57:46 -0500

    Added Iterator inner class

--- 02SuperArrayIterator/SuperArray.java
+++ 02SuperArrayIterator/SuperArray.java
@@ -1,3 +1,5 @@
+import java.util.Iterator;
+
 public class SuperArray{
     private String[] data;
     private int size;
@@ -204,4 +206,31 @@ public class SuperArray{
     return n;
     }
   }
+    public static class SuperArrayIterator implements Iterator<String>{
+    private SuperArray s;
+    private int index;
+ 
+    public SuperArrayIterator(SuperArray sa){
+	index = 0;
+	s = sa;
+    }
+    public boolean hasNext(){
+	return (index < s.size());
+    }
+    public String next(){
+    if(hasNext()){
+        index++;
+        return s.get(index - 1);
+        }
+    else{
+        throw new IndexOutOfBoundsException();
+    }
+    }
+    public void Remove(){
+	throw new UnsupportedOperationException("Can not remove");
+    }
+    public Iterator<String> iterator(){
+        return new SuperArrayIterator(s);
+    }
+}
 }
